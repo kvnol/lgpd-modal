b4922517ddffcb37da69a2243d4ec340a61eee10
Kevin Oliveira <me@kevinoliveira.com.br>
2021-08-14 18:01:31 -0300

    refactor: Adicionando mais formatos de módulos
    - Adição dos formatos UMD e ES; - Alteração na documentação do
    package.json

---
+++ dist/lgpdmodal.esm.js
@@ -0,0 +1,80 @@
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
+var template = function template(acceptPolicy, url) {
+  return "\n  <div class=\"modal-lgpd ".concat(acceptPolicy ? '' : '-show', "\">\n    <div class=\"bg\"></div>\n    <div class=\"dialog\">\n      <h4 class=\"title\">Configura\xE7\xE3o de Cookies:</h4>\n      <p class=\"description\">Usamos cookies para lhe oferecer uma experi\xEAncia de navega\xE7\xE3o melhor, analisar o tr\xE1fego do site e personalizar o conte\xFAdo. Para saber mais sobre cookies acesse nossa <a href=\"").concat(url || '/politica-de-privacidade/', "\" target=\"_blank\" title=\"Acesse a p\xE1gina com informa\xE7\xF5es de privacidade\">Pol\xEDtica de Privacidade</a>. Para aceitar, clique no bot\xE3o \"aceitar cookies\".</p>\n\n      <div class=\"actions\">\n        <button\n          class=\"button -filled\"\n          data-cookies=\"accept\"\n        >\n          <span>Aceitar cookies</span>\n        </button>\n      </div>\n    </div>\n  </div>\n");
+};
+
+var LGPDModal = /*#__PURE__*/function () {
+  /**
+   * LGPDModal Constructor
+   * @constructor LGPDModal
+   */
+  function LGPDModal(options) {
+    _classCallCheck(this, LGPDModal);
+
+    this.initModal(options);
+  }
+
+  _createClass(LGPDModal, [{
+    key: "initModal",
+    value: function initModal(options) {
+      this.$root = document.body;
+      var acceptPolicy = !!window.localStorage.getItem('acceptPolicy');
+      var $dialog = template(acceptPolicy, options.url);
+
+      if (!acceptPolicy) {
+        this.$root.insertAdjacentHTML('beforeend', $dialog);
+        this.onAccept(options.scripts);
+      }
+    }
+  }, {
+    key: "onAccept",
+    value: function onAccept(scripts) {
+      var _this = this;
+
+      var $modal = document.querySelector('.modal-lgpd');
+      var $button = document.querySelector('[data-cookies="accept"]');
+      $button === null || $button === void 0 ? void 0 : $button.addEventListener('click', function () {
+        window.localStorage.setItem('acceptPolicy', true);
+        $modal === null || $modal === void 0 ? void 0 : $modal.classList.remove('-show');
+
+        if (scripts) {
+          _this.includeScripts(scripts);
+        }
+      });
+    }
+  }, {
+    key: "includeScripts",
+    value: function includeScripts(scripts) {
+      if (this.acceptPolicy) {
+        scripts.forEach(function (script) {
+          document.head.insertAdjacentHTML('beforeend', script);
+        });
+      }
+    }
+  }]);
+
+  return LGPDModal;
+}();
+
+export { LGPDModal as default };
--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,40 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
 
-export default {
+const config = [];
+
+config.push({
   input: 'src/js/index.js',
   output: {
     name: 'LGPDModal',
-    file: 'dist/index.js',
+    file: 'dist/lgpdmodal.umd.js',
     format: 'umd',
+    extend: true,
+    esModule: 'false',
+    exports: 'named',
   },
   plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
-};
+});
+
+config.push({
+  input: 'src/js/index.js',
+  output: {
+    name: 'LGPDModal',
+    file: 'dist/lgpdmodal.esm.js',
+    format: 'es',
+    exports: 'named',
+  },
+  plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
+});
+
+config.push({
+  input: 'src/js/index.js',
+  output: {
+    name: 'LGPDModal',
+    file: 'dist/lgpdmodal.min.js',
+    format: 'iife',
+  },
+  plugins: [resolve(), babel({ babelHelpers: 'bundled' })],
+});
+
+export default config;
